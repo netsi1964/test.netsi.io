0547822b0de1b57655300950109849c6be0966de
Sten Hougaard <netsi1964@gmail.com>
2015-12-08 22:19:06 +0100

    added handlebars

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # test.netsi.io
 The code from [test.netsi.io](http://test.netsi.io)
+
+## log
+1.  8. dec. 2015: Added support for handlebars to the Express engine (Read this: [http://webapplog.com/jade-handlebars-express/](http://webapplog.com/jade-handlebars-express/) )
+  1. Using this package: [https://github.com/ericf/express-handlebars](https://github.com/ericf/express-handlebars)
+2.  Installing AngulaJS using Yeoman. [https://github.com/yeoman/generator-angular#readme](https://github.com/yeoman/generator-angular#readme) 
--- index.js
+++ index.js
@@ -1,15 +1,59 @@
 var express = require("express");
 var app = express();
+var cookieParser = require('cookie-parser');
+
 var netsiSuncalc = require("./netsi_suncalc.js");
+var location = require("./location.js");
 
 app.set("port", (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
+app.use(cookieParser());
 
-app.get("/", function(request, response) {
-  var result = netsiSuncalc();
-  // response.setHeader("Content-Type", "text/html");
-  response.send(result);
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+
+app.get("/", function(req, res) {
+  //var result = netsiSuncalc();
+  // res.setHeader("Content-Type", "text/html");
+  res.render('map', {
+    location: req.cookies.location
+  });
+  //res.send(result);
 });
+
+app.get("/location", function(req, res) {
+  if (!req.cookies.location) {
+    location(function(location) {
+      if (!location) {
+        res.status("400").json({
+          error: "Could not get location"
+        });
+      } else {
+        if (location) {
+          var temp = location.loc.split(",").map(function(e) {
+            return parseFloat(e);
+          });
+          location.loc = {"lat":temp[0], "lng":temp[1]};
+        }
+        res.cookie('location', location, {
+          maxAge: 60 * 1000 * 60 * 24 * 3
+        });
+        location.fromCookie = false;
+        res.json(location);
+      }
+    });
+  } else {
+    var loc = req.cookies.location;
+    loc.fromCookie = true;
+  }
+  res.json(loc);
+});
+
+
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running on port", app.get("port"));
